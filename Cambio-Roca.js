34b15e84da3bfc743e1f7d558efb41ff28f6f529
Lucas Nicolosi <you@example.com>
2023-11-27 11:21:25 -0300

    cambios en compra con fetch

--- Cambio-Roca.js
+++ Cambio-Roca.js
@@ -1,122 +1,130 @@
+function Usuario(nombre, pass) {
+    this.nombre = nombre;
+    this.pass = pass;
+  }
 
-// function usuarios (nombre,pass){
-     
-//     this.nombre=nombre;
-//     this.pass=pass
+  const usuariosRegistrados = JSON.parse(localStorage.getItem("registroUsu")) || [];
 
+  function iniciarSesion() {
+    const nombre = document.getElementById("nombre").value;
+    const pass = document.getElementById("pass").value;
 
-// }
-// const usuariosRegistrados = [
-//     {   nombre:"china",
-//         pass: 2023
+    const usuarioEncontrado = usuariosRegistrados.find(usu => usu.nombre === nombre && usu.pass === pass);
 
-// }
-// ]
-// function iniciarSecion(nombre,pass){    
-//     const usuarioSell= usuariosRegistrados.find((usu)=>usu.nombre===nombre&&usu.pass===pass)
-//     if(usuarioSell){
-//         alert("bienbenido"+ usuarioSell)
-//     }else{
-//         alert("usuario incorrecto")
-//     }
+    if (usuarioEncontrado) {
+      Swal.fire({
+        title: "¡Bienvenido!",
+        text: "¿Desea realizar una compra?",
+        showCancelButton: true,
+        confirmButtonText: "Sí",
+        cancelButtonText: "No"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          compra()
+          realizarCompra();
+        }
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Usuario Incorrecto!!!",
+        text: "Ingresa un usuario válido.",
+        footer: 'Te equivocaste salame'
+      });
+    }
+  }
 
+  function agregarUsuario() {
+    const nuevoNombre = document.getElementById("nuevoNombre").value;
+    const nuevoPass = document.getElementById("nuevoPass").value;
 
-// }
-// function agregarUsuario(nombre,pass){
-//      if(nombre!=" "||pass!=""){
-//         alert("igresa datos validos")
-//      }
+    if (nuevoNombre !== "" && nuevoPass !== "") {
+      const nuevoUsuario = new Usuario(nuevoNombre, nuevoPass);
+      usuariosRegistrados.push(nuevoUsuario);
+      localStorage.setItem("registroUsu", JSON.stringify(usuariosRegistrados));
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Usuario creado!",
+        showConfirmButton: false,
+        timer: 1800
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Usuario Incorrecto!!!",
+        text: "Ingresa un usuario válido.",
+        footer: 'Te equivocaste salame'
+      });
+    }
+  }
 
-//      const nuevoUsuario={
-//         nombre,
-//         pass
-//      }
-//      nuevoUsuario.push(nuevoUsuario)
+  // function compra() {
+  //    const url="https://dolarapi.com/v1/dolares/blue"
 
-//      localStorage.setItem("registroUsu",JSON.stringify(nuevoUsuario))
-//      return true;
-
-// }
-
-
-
-
-
-
-
-// // const url="https://www.dolarsi.com/api/api.php?type=valoresprincipales"
-
-// // fetch(url)
-// // .then((respuesta)=> respuesta.json())
-// // .then((data)=>console.table(data))
-
-// const url = "https://www.dolarsi.com/api/api.php?type=valoresprincipales";
-
-// const containerCambio= document.getElementById("container-cambio")
-
-// fetch(url)
-//   .then((respuesta) => respuesta.json())
-//   .then((data) => {
-//     // Obtener el contenedor en el que se agregarán las tarjetas
-//     const containerCambio = document.getElementById("container-cambio");
-
-//     // Iterar sobre cada valor del dólar
-//     data.forEach((dolar) => {
-//       // Crear elementos HTML
-//       const card = document.createElement("div");
-//       const head = document.createElement("h2");
-//       const parra = document.createElement("p");
-
-//       // Configurar contenido de los elementos
-//       head.textContent = dolar.nombre;
-//       parra.textContent = `Compra: ${dolar.compra} Venta: ${dolar.venta}`;
-
-//       // Agregar elementos a la tarjeta
-//       card.appendChild(head);
-//       card.appendChild(parra);
-
-//       // Agregar tarjeta al contenedor
-//       containerCambio.appendChild(card);
-//     });
-//   })
-//   .catch((error) => console.error("Error al obtener los datos:", error));
-
-
-
-
+  //   fetch(url)
+  //   .then((respuesta)=> respuesta.json())
+  //   .then((data)=>console.table(data))
     
-        function Usuario(nombre, pass) {
-            this.nombre = nombre;
-            this.pass = pass;
+  //   fetch(url)
+  //     .then((respuesta) => respuesta.json())
+  //     .then((data) => {
+        
+          
+  //     })
+  //    .catch((error) => console.error("Error al obtener los datos:", error));
+  //   }
+
+
+
+
+          
+            
+        function compra() {
+          const url="https://dolarapi.com/v1/dolares/blue"
+          const containerCompra = document.getElementById("container-compra");
+          fetch(url)
+         .then((respuesta)=> respuesta.json())
+         .then((data)=>console.table(data))
+    
+          fetch(url)
+         .then((respuesta) => respuesta.json())
+         .then((data) => { 
+          const h1 = document.createElement("h1");
+         const labelDolar = document.createElement("label");
+         const inputDolar = document.createElement("input");
+         const labelPesos = document.createElement("label");
+         const inputPesos = document.createElement("input");
+         const botonCompra = document.createElement("button");
+        
+         h1.textContent = "Compra de dólares";
+         labelDolar.textContent = "Cantidad de dólar a comprar:";
+         labelPesos.textContent = "Cantidad en pesos argentinos:";
+         inputDolar.type = "number";
+         inputPesos.type = "number";
+         botonCompra.textContent = "Realizar compra";
+         botonCompra.addEventListener("click", realizarCompra);
+
+         containerCompra.appendChild(h1);
+         containerCompra.appendChild(labelDolar);
+         containerCompra.appendChild(inputDolar);
+         containerCompra.appendChild(labelPesos);
+         containerCompra.appendChild(inputPesos);
+         containerCompra.appendChild(botonCompra);
+
+
+
+
+          })
+          .catch((error) => console.error("Error al obtener los datos:", error));
         }
-
-        const usuariosRegistrados = [
-            { nombre: "china", pass: "2023" }
-        ];
-
-        function iniciarSesion() {
-            const nombre = document.getElementById("nombre").value;
-            const pass = document.getElementById("pass").value;
-
-            const usuarioEncontrado = usuariosRegistrados.find(usu => usu.nombre === nombre && usu.pass === pass);
-
-            if (usuarioEncontrado) {
-                alert("¡Bienvenido, " + usuarioEncontrado.nombre + "!");
-            } else {
-                alert("Usuario incorrecto");
-            }
-        }
-
-        function agregarUsuario() {
-            const nuevoNombre = document.getElementById("nuevoNombre").value;
-            const nuevoPass = document.getElementById("nuevoPass").value;
-
-            if (nuevoNombre !== "" && nuevoPass !== "") {
-                const nuevoUsuario = new Usuario(nuevoNombre, nuevoPass);
-                usuariosRegistrados.push(nuevoUsuario);
-                localStorage.setItem("registroUsu", JSON.stringify(usuariosRegistrados));
-                alert("Usuario agregado correctamente");
-            } else {
-                alert("Ingresa datos válidos");
-            }
-        }
+      function realizarCompra() {
+      montoDolar= inputDolar.value
+      montoArg= inputPesos.value
+      total= montoArg / montoDolar
+    
+      console.log("Compra realizada");
+    }
+      
+            
+        
